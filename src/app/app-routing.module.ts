62afb3aa522faa60d1b81461550b725aa00dc1fd
lm902 <i@lm902.cn>
2022-12-02 20:35:36 -0800

    配置路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AboutComponent } from './views/about/about.component'
+import { DetailComponent } from './views/detail/detail.component'
+import { DiscoverComponent } from './views/discover/discover.component'
+import { FavoritesComponent } from './views/favorites/favorites.component'
+import { HomeComponent } from './views/home/home.component'
+import { RatedComponent } from './views/rated/rated.component'
+import { SearchComponent } from './views/search/search.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { pathMatch: 'full', path: '/', component: HomeComponent },
+  { path: '/about', component: AboutComponent },
+  { path: '/search/:query', component: SearchComponent },
+  { path: '/discover', component: DiscoverComponent },
+  { path: '/detail/:id', component: DetailComponent },
+  { path: '/favorites', component: FavoritesComponent },
+  { path: '/rated', component: RatedComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
