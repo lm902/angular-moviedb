bfc5ea421822bb91f3e2b2dd96422bde84880a45
lm902 <i@lm902.cn>
2022-12-03 20:39:08 -0800

    实现评分和收藏按钮

--- src/app/partials/favorite-button/favorite-button.component.ts
+++ src/app/partials/favorite-button/favorite-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import MovieDetailResultObject from 'src/app/models/MovieDetailResultObject'
+import { favorites } from '../../models/UserCustomization'
+import locale from '../../locales/LocaleImports'
 
 @Component({
   selector: 'app-favorite-button',
@@ -8,6 +10,19 @@ import MovieDetailResultObject from 'src/app/models/MovieDetailResultObject'
 })
 export class FavoriteButtonComponent implements OnInit {
   @Input() movie: MovieDetailResultObject | null = null
+  readonly locale = locale
+
+  get favorited (): boolean {
+    if (this.movie == null) return false
+    return favorites.has(this.movie)
+  }
+
+  click (): void {
+    if (this.movie == null) return
+    this.favorited
+      ? favorites.remove(this.movie)
+      : favorites.add(this.movie)
+  }
 
   ngOnInit (): void {
   }
--- src/app/partials/rate-button/rate-button.component.ts
+++ src/app/partials/rate-button/rate-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import MovieDetailResultObject from 'src/app/models/MovieDetailResultObject'
+import { liked, disliked } from '../../models/UserCustomization'
+import locale from '../../locales/LocaleImports'
 
 @Component({
   selector: 'app-rate-button',
@@ -8,6 +10,33 @@ import MovieDetailResultObject from 'src/app/models/MovieDetailResultObject'
 })
 export class RateButtonComponent implements OnInit {
   @Input() movie: MovieDetailResultObject | null = null
+  readonly locale = locale
+
+  get liked (): boolean {
+    if (this.movie == null) return false
+    return liked.has(this.movie)
+  }
+
+  get disliked (): boolean {
+    if (this.movie == null) return false
+    return disliked.has(this.movie)
+  }
+
+  click (btn: 'liked' | 'disliked'): void {
+    if (this.movie == null) return
+    switch (btn) {
+      case 'liked':
+        this.liked
+          ? liked.remove(this.movie)
+          : liked.add(this.movie)
+        break
+      case 'disliked':
+        this.disliked
+          ? disliked.remove(this.movie)
+          : disliked.add(this.movie)
+        break
+    }
+  }
 
   ngOnInit (): void {
   }
